ae92f72e00bb195a1c8a0a79775a99b4beffcd70
Hans van Luttikhuizen <hansvanluttikhuizen@me.com>
2018-12-03 15:03:23 +0100

    Solve day 2

--- Day 2 - Inventory Management System/src/InventoryManagementSystem.kt
+++ Day 2 - Inventory Management System/src/InventoryManagementSystem.kt
@@ -1,5 +1,18 @@
 package nl.pindab0ter.aoc2018.day2
 
 fun main(args: Array<String>) {
-    println("Inventory Management System")
+    val boxes = ClassLoader
+        .getSystemResource("input")
+        .readText(Charsets.UTF_8)
+        .lines()
+
+    println(boxes.appearancesFor(2) * boxes.appearancesFor(3))
+}
+
+fun List<String>.appearancesFor(n: Int): Int = this
+    .filter { it.hasCharsAppearing(n) }
+    .count()
+
+fun String.hasCharsAppearing(n: Int) = fold(false) { acc, c ->
+    this.count { it == c } == n || acc
 }
