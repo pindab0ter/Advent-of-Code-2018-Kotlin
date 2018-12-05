9ae7d8bc042cabbc2e8ad4ba1d1337297da0b8ad
Hans van Luttikhuizen <hansvanluttikhuizen@me.com>
2018-12-05 21:30:46 +0100

    Add unit tests for Day 2

---
+++ Day 2 - Inventory Management System/src/InventoryManagementSystemTest.kt
@@ -0,0 +1,21 @@
+package nl.pindab0ter.aoc2018.day2
+
+import kotlin.test.assertEquals
+
+internal class InventoryManagementSystemTest {
+
+    @org.junit.Test
+    fun boxCount() {
+        val input = listOf("abcdef", "bababc", "abbcde", "abcccd", "aabcdd", "abcdee", "ababab")
+        val actual = boxCount(input)
+        assertEquals(actual, 12)
+    }
+
+    @org.junit.Test
+    fun findBoxes() {
+        val input = listOf("abcde", "fghij", "klmno", "pqrst", "fguij", "axcye", "wvxyz")
+        val actual = findBoxes(input)
+        assertEquals(actual, "fgij")
+    }
+
+}
