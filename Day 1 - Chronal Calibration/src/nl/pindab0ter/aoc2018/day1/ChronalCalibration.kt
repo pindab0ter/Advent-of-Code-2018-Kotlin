03279a1d44b38567adf0b1f692e46154766eec3a
Hans van Luttikhuizen <hansvanluttikhuizen@me.com>
2018-12-06 13:33:25 +0100

    Change main function for REPL compatibility

--- Day 1 - Chronal Calibration/src/nl/pindab0ter/aoc2018/day1/ChronalCalibration.kt
+++ Day 1 - Chronal Calibration/src/nl/pindab0ter/aoc2018/day1/ChronalCalibration.kt
@@ -1,20 +1,22 @@
 package nl.pindab0ter.aoc2018.day1
 
-val input: List<Int> = ClassLoader
+fun main(args: Array<String>) = ClassLoader
     .getSystemResource("input")
     .readText(Charsets.UTF_8)
     .lines()
     .map(String::toInt)
+    .let { input ->
+        print("""
+            Day 1: Chronal Calibrations
 
-fun main(args: Array<String>) = print("""
-    Day 1: Chronal Calibrations
+            Part one: Starting with a frequency of zero, what is the resulting frequency after all of the changes in frequency have been applied?
+            ${findFinalFrequency(input)}
 
-    Part one: Starting with a frequency of zero, what is the resulting frequency after all of the changes in frequency have been applied?
-    ${findFinalFrequency(input)}
-
-    Part two: What is the first frequency your device reaches twice?
-    ${findFirstRepeatedFrequency(input)}
-    """.trimIndent())
+            Part two: What is the first frequency your device reaches twice?
+            ${findFirstRepeatedFrequency(input)}
+            """.trimIndent()
+        )
+    }
 
 fun findFinalFrequency(input: List<Int>): Int = input.sum()
 
--- Day 2 - Inventory Management System/src/nl/pindab0ter/aoc2018/day2/InventoryManagementSystem.kt
+++ Day 2 - Inventory Management System/src/nl/pindab0ter/aoc2018/day2/InventoryManagementSystem.kt
@@ -1,19 +1,20 @@
 package nl.pindab0ter.aoc2018.day2
 
-val input = ClassLoader
+fun main(args: Array<String>) = ClassLoader
     .getSystemResource("input")
     .readText(Charsets.UTF_8)
     .lines()
+    .let { input ->
+        print("""
+            Day 2: Inventory Management System
 
-fun main(args: Array<String>) = print("""
-    Day 2: Inventory Management System
-
-    Part one: What is the checksum for your list of box IDs?
-    ${boxCount(input)}
-
-    What letters are common between the two correct box IDs?
-    ${findBoxes(input)}
-    """.trimIndent())
+            Part one: What is the checksum for your list of box IDs?
+            ${boxCount(input)}
+            What letters are common between the two correct box IDs?
+            ${findBoxes(input)}
+            """.trimIndent()
+        )
+    }
 
 fun boxCount(boxes: List<String>): Int {
     return count(boxes, 2) * count(boxes, 3)
