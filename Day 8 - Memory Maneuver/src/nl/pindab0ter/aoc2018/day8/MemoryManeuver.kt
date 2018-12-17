a80bef74f39cc3ec787be4e48a60e6507b5105c9
Hans van Luttikhuizen <hansvanluttikhuizen@me.com>
2018-12-17 12:17:21 +0100

    Solve day 8 part one

--- Day 8 - Memory Maneuver/src/nl/pindab0ter/aoc2018/day8/MemoryManeuver.kt
+++ Day 8 - Memory Maneuver/src/nl/pindab0ter/aoc2018/day8/MemoryManeuver.kt
@@ -4,6 +4,7 @@ fun main(args: Array<String>) = ClassLoader
     .getSystemResource("input")
     .readText(Charsets.UTF_8)
     .split(" ")
+    .map(String::toInt)
     .let { input ->
         print("""
             --- Day 8: Memory Maneuver ---
@@ -16,4 +17,26 @@ fun main(args: Array<String>) = ClassLoader
         )
     }
 
-fun sumOfEntries(input: List<String>): Int = TODO()
+data class Node(val children: List<Node>, val metadata: List<Int>) {
+    val size: Int get() = 2 + children.sumBy { it.size } + metadata.size
+    val metaDataSum: Int get() = children.sumBy { it.metaDataSum } + metadata.sum()
+}
+
+fun parse(input: List<Int>): Node {
+    val amountOfChildren = input[0]
+    val amountOfMetadata = input[1]
+    val remainder = input.drop(2)
+
+    val children: List<Node> =
+        if (amountOfChildren == 0) emptyList()
+        else (0 until amountOfChildren).fold(emptyList()) { children: List<Node>, _: Int ->
+            val thisRemainder = remainder.drop(children.sumBy { it.size })
+            children + parse(thisRemainder)
+        }
+
+    val metadata: List<Int> = remainder
+        .drop(children.sumBy { it.size })
+        .take(amountOfMetadata)
+
+    return Node(children, metadata)
+}
--- Day 8 - Memory Maneuver/src/nl/pindab0ter/aoc2018/day8/MemoryManeuverTest.kt
+++ Day 8 - Memory Maneuver/src/nl/pindab0ter/aoc2018/day8/MemoryManeuverTest.kt
@@ -5,11 +5,11 @@ import kotlin.test.assertEquals
 
 class MemoryManeuverTest {
 
-    private val input = listOf("2", "3", "0", "3", "10", "11", "12", "1", "1", "0", "1", "99", "2", "1", "1", "2")
+    private val input = listOf(2, 3, 0, 3, 10, 11, 12, 1, 1, 0, 1, 99, 2, 1, 1, 2)
 
     @Test
     fun correctOrder() {
-        val actual = sumOfEntries(input)
+        val actual = parse(input).metaDataSum
         assertEquals(138, actual)
     }
 
